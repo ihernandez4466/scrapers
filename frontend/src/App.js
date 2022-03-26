e275d0a467a95b5cd2d7b55dacfabc550f8674b3
Isabel Hernandez <ihernandez44@csuchico.edu>
2022-03-26 12:57:55 -0700

    package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
+import { Box, Text, Header, Button, Icons, Menu } from 'grommet';
+import { Home } from 'grommet-icons';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box direction="column">
+      <Header background="brand">
+        <Button icon={<Home />} hoverIndicator />
+        <Menu label="account" items={[{ label: 'logout' }]} />
+      </Header>
+    </Box>
   );
 }
 
